98008306a6f532fa3c9e25aac7a4ccc8c5a4b931
brianschultz <brianschultz320@gmail.com>
2026-06-19 16:26:27 -0300

    refactor: integrate FlatDarkLaf

--- src/main/java/com/example/klebao/KlebaoApplication.java
+++ src/main/java/com/example/klebao/KlebaoApplication.java
@@ -1,9 +1,15 @@
 package com.example.klebao;
 
+import com.formdev.flatlaf.FlatDarkLaf;
+
+import java.awt.*;
+
 public class KlebaoApplication {
 
     public static void main(String[] args) {
-        java.awt.EventQueue.invokeLater(() -> {
+        FlatDarkLaf.setup();
+
+        EventQueue.invokeLater(() -> {
             LauncherGUI frame = new LauncherGUI();
             frame.setVisible(true);
         });
