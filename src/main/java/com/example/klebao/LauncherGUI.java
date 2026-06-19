fe95b8a573b00c9c0913c1e28016c758e2558b24
brianschultz <brianschultz320@gmail.com>
2026-06-19 17:02:10 -0300

    refactor: GUI layout

--- src/main/java/com/example/klebao/LauncherGUI.java
+++ src/main/java/com/example/klebao/LauncherGUI.java
@@ -6,53 +6,68 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.File;
 
+/** Arquivo de configuração da interface gráfica do usuário (GUI) para o projeto.
+ * @author Brian Schultz EQ03 * @version 1.0 * @since 2026 */
 public class LauncherGUI extends JFrame {
 
     private File selectedFile;
 
     public LauncherGUI() {
-        setTitle("Klebão - Analisador Léxico & Tabela De Símbolos");
+        setTitle("Klebão2026-1 - Analisador Léxico e Tabela De Símbolos");
         setSize(600, 400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
         JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BorderLayout());
-        mainPanel.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20)); // Padding
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
-        JLabel headerLabel = new JLabel("<html><div style='text-align:center;'>" + "<h2 style='color:#64B4FF;'>Analisador Léxico & Tabela De Símbolos</h2>" + "<b style='text-align:center; font-weight:bold; font-size:13px; margin-bottom:0; padding-bottom:0;'>Código da Equipe:</b> EQ03<br>" + "<b style='text-align:center; font-weight:bold; font-size:13px; margin-bottom:0; padding-bottom:0;'>Componentes:</b><br>" + "Brian Friedrich dos Santos Schultz; brianschultz320@gmail.com; 71986300394<br>" + "Guilherme Ferreira Sampaio<br>" + "Samuel Pereira dos Santos Santana<br>" + "Olivier Teles Leal Araujo" + "</div></html>");
+        JLabel headerLabel = new JLabel("<html><div style='text-align:center;'>" + "<h2 " +
+                                        "style='color:#64B4FF;'>Analisador Léxico & Tabela De " +
+                                        "Símbolos</h2>" + "<b style='text-align:center; " +
+                                        "font-weight:bold; font-size:13px; margin-bottom:0; " +
+                                        "padding-bottom:0;'>Código da Equipe:</b> EQ03<br>" + "<b" +
+                                        " style='text-align:center; font-weight:bold; " +
+                                        "font-size:13px; margin-bottom:0; padding-bottom:0;" +
+                                        "'>Componentes:</b><br>" + "Brian Friedrich dos Santos " +
+                                        "Schultz; brian.schultz@ucsal.edu.br; 71986300394<br>" +
+                                        "Guilherme Ferreira Sampaio; guilhermeferreira.sampaio@ucsal.edu.br;" +
+                                        " " +
+                                        " <br>" + "Samuel Pereira dos Santos Santana; samuel.santana@ucsal.edu.br<br>" +
+                                        "Olivier Teles Leal Araujo; olivier.araujo@ucsal.edu" +
+                                                                     ".br" + "</div></html>");
         mainPanel.add(headerLabel, BorderLayout.NORTH);
 
         JPanel centerPanel = new JPanel();
         centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
         centerPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
 
-        JPanel instructionsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));  // Alinha totalmente à esquerda, sem margem lateral
+        JPanel instructionsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
 
-        JLabel instructionsLabel = new JLabel("<html>" + "<div style='text-align:center; font-weight:bold; font-size:13px; margin-bottom:0; padding-bottom:0;'>Instruções:</div>" + "<div style='margin-top:4px;'>" + "- Clique em 'Escolher Arquivo' para selecionar um " + "arquivo de código-fonte com extensão .261<br>" + "- Após selecionar, clique em 'Executar Análise' para iniciar o processamento.<br>" + "- Os arquivos de saída (.LEX e .TAB) serão gerados na pasta /output do projeto." + "</div>" + "</html>");
-
-
-        instructionsPanel.add(instructionsLabel);
-        centerPanel.add(instructionsPanel);
+        JLabel instructionsLabel = new JLabel("<html>" + "<div style='text-align:center; " +
+                                              "font-weight:bold; font-size:13px; margin-bottom:0;" +
+                                              " padding-bottom:0;'>Instruções:</div>" + "<div " +
+                                              "style='margin-top:4px;'>" + "1 - Clique em " +
+                                              "'Escolher Arquivo' para selecionar um " + "arquivo" +
+                                              " de código-fonte com extensão .261<br>" + "2 - " +
+                                              "Após selecionar, clique em 'Executar Análise' para" +
+                                              " iniciar o processamento.<br>" + " 3 - Os arquivos de saída (.LEX e .TAB) serão gerados na pasta /output do projeto." + "</div>" + "</html>");
 
         JLabel filePathLabel = new JLabel(" ");
-        filePathLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        centerPanel.add(Box.createVerticalStrut(10));
-        centerPanel.add(filePathLabel);
-
-        JPanel buttonsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 0));
-
+        JPanel buttonsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 5));
         JButton chooseButton = new JButton("Escolher Arquivo .261");
         JButton runButton = new JButton("Executar Análise");
 
-        buttonsPanel.add(chooseButton);
-        buttonsPanel.add(runButton);
+        filePathLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         centerPanel.add(Box.createVerticalStrut(10));
+        centerPanel.add(filePathLabel);
+        buttonsPanel.add(chooseButton);
+        buttonsPanel.add(runButton);
+        instructionsPanel.add(instructionsLabel);
+        centerPanel.add(instructionsPanel);
+        centerPanel.add(Box.createVerticalStrut(10));
         centerPanel.add(buttonsPanel);
-
         mainPanel.add(centerPanel, BorderLayout.CENTER);
-
         add(mainPanel);
 
         chooseButton.addActionListener(e -> {
